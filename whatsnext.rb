0e25919da67511b97a3f107d8605ac64ddeffd9e
Dave Huffman <daveh303@yahoo.com>
2013-07-13 18:18:18 -0400

    Added usage.

--- whatsnext.rb
+++ whatsnext.rb
@@ -1,1 +1,6 @@
-/Users/davehuffman/code/scripts
+#!/usr/bin/env ruby
+
+if ARGV.first == '--help' || ARGV.first == '-h'
+  puts 'Print the next task for each project directory found.'
+  puts "usage: #{File.basename(__FILE__)} [directory]"
+end
